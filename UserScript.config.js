444489650022f377fd7ff4f96b004e0bd8872b6e
Lin <i@linlin.me>
2023-05-10 13:01:34 +0800

    feat!: 从 esbuild 切换到基于 vite-plugin-monkey 开发

--- UserScript.config.js
+++ UserScript.config.js
@@ -6,37 +6,32 @@ const globalConfig = {
     source: 'https://github.com/LinLin00000000/L-UserScript',
 }
 
-const userScriptConfig = [
-    {
-        fileName: 'Feishu-Mindnote-Helper.js',
+const userScriptConfig = {
+    'Feishu Mindnote Helper': {
         description:
             'Hide some useless elements in Feishu Mindnote page, make it clean and tidy.',
-        matches: [
+        match: [
             'https://linlin00.feishu.cn/mindnotes/EwaGbXd70mcocjnqRu9cbDO7nOc',
         ],
         icon: 'https://www.google.com/s2/favicons?sz=64&domain=feishu.cn',
     },
-    {
+    'NTDM Helper': {
+        match: ['*.ntdm8.com/*', 'danmu.yhdmjx.com/*'],
+    },
+    test: {
         fileName: 'test.js',
         description: 'test',
     },
-]
-
-export const userScripts = userScriptConfig.map(script => {
-    const scriptConfig = {
-        ...globalConfig,
-        ...script,
-    }
-
-    scriptConfig.name =
-        scriptConfig.name || fileNameCoverter(scriptConfig.fileName)
-
-    return scriptConfig
-})
-
-/**
- * @param {string} fileName
- */
-function fileNameCoverter(fileName) {
-    return fileName.replace(/-/g, ' ').replace(/\.js$/, '')
 }
+
+const userScripts = {}
+for (const [name, config] of Object.entries(userScriptConfig)) {
+    userScripts[name] = {
+        ...globalConfig,
+        name,
+        source: `${globalConfig.source}/tree/main/${name}/src/main.js`,
+        ...config,
+    }
+}
+
+export { userScripts }
---
+++ utils.js
@@ -0,0 +1,27 @@
+export function getDirname(importMeta) {
+    return decodeURI(importMeta.url).split(/[\\/]/).filter(Boolean).at(-2)
+}
+
+/**
+ * @description Group an array by a function
+ * @template T
+ * @param {T[]} array
+ * @param {(item: T) => boolean} f 将数组中的每一项传入该函数，返回 true 则放入第一个数组，否则放入第二个数组
+ * @returns {[T[], T[]]}
+ * @example
+ * const [trueArray, falseArray] = groupBy([1, 2, 3, 4, 5], item => item % 2 === 0)
+ * // trueArray = [2, 4]
+ * // falseArray = [1, 3, 5]
+ */
+export function groupBy(array, f) {
+    const trueArray = []
+    const falseArray = []
+    array.forEach(item => {
+        if (f(item)) {
+            trueArray.push(item)
+        } else {
+            falseArray.push(item)
+        }
+    })
+    return [trueArray, falseArray]
+}
