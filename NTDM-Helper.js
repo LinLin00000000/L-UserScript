b3c1133422ddfc0d6cbe8cd961584b896de38452
Lin <i@linlin.me>
2023-05-14 15:06:16 +0800

    chore: 删除一些无用的代码

--- NTDM-Helper.js
+++ NTDM-Helper.js
@@ -25,20 +25,12 @@ if (url) {
     })
 }
 
-// window.addEventListener('message', log)
-messageEmitter.addListener((message, e) => {
-    console.log(`selfOringin: ${window.location.origin}`)
-    console.log(`message: ${message}`)
-    console.log(`originEvent: `, e)
-})
-
 progressiveQuery(['#container > p'], e => {
     e.insertAdjacentHTML(
         'beforebegin',
         `<button type="button" id="debug" >debug</button>`
     )
     document.querySelector('#debug').onclick = () => {
-        postMessageTest()
         console.log('debug')
     }
 })
@@ -48,18 +40,7 @@ progressiveQuery(['#bofang'], e => {
         'afterend',
         `<button class="leleplayer-icon leleplayer-play-icon" id="next-video"><svg class="squirtle-svg-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22"><path d="M16 5a1 1 0 00-1 1v4.615a1.431 1.431 0 00-.615-.829L7.21 5.23A1.439 1.439 0 005 6.445v9.11a1.44 1.44 0 002.21 1.215l7.175-4.555a1.436 1.436 0 00.616-.828V16a1 1 0 002 0V6C17 5.448 16.552 5 16 5z"></path></svg></button>`
     )
-    document.querySelector('#next-video').addEventListener('click', nextVideo)
+    document.querySelector('#next-video').addEventListener('click', () => {
+        messageEmitter.emitMessage('nextVideo')
+    })
 })
-
-function nextVideo() {
-    messageEmitter.emitMessage('nextVideo')
-}
-
-function log(e) {
-    console.log(e)
-    console.log(`window.location ${window.location}`)
-}
-
-function postMessageTest(message = 'debug') {
-    messageEmitter.emitMessage(message)
-}
