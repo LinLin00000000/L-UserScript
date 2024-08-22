e07f38769cca976a659c81949b8ef1a93edb8db5
Lin <i@linlin.me>
2024-08-22 22:36:56 +0800

    feat: Add listenerWithTimeout function for mouse movement detection
    And format code

--- AIzex-Helper.js
+++ AIzex-Helper.js
@@ -1,60 +1,77 @@
 import {
-    dynamicQuery,
-    isEmptyString,
-    mybuild,
-    textQuery,
-    onUrlChange,
+  dynamicQuery,
+  isEmptyString,
+  mybuild,
+  textQuery,
+  onUrlChange,
+  listenerWithTimeout,
+  extractUniqueHosts,
 } from './utils'
 
+const mainPageUrl = 'https://aizex.net/'
+const gptPoolUrl = 'https://aizex.net/plusPool'
+const gptChatUrl = 'https://linlin.aizex.cn/'
+const claudePoolUrl = 'https://aizex.net/proPool'
+const claudeChatUrl = 'https://linlin-c.aizex.cn/'
+
 await mybuild(
-    {
-        match: ['panter.aizex.cn/*', 'aizex.net/*'],
-    },
-    {
-        // dev: true,
-    }
+  {
+    match: extractUniqueHosts([
+      mainPageUrl,
+      gptPoolUrl,
+      gptChatUrl,
+      claudePoolUrl,
+      claudeChatUrl,
+    ]),
+  },
+  {
+    dev: false,
+  }
 )
 
-if (location.host.includes('panter.aizex.cn')) {
-    // 对话页面
+if (location.href.includes(mainPageUrl)) {
+  // Aizex 合租面板
 
-    // 如果未登录，则会有一个全局的重定向方法，用于重定向到首页
-    setTimeout(() => {
-        if (window.redirectToHomePage) {
-            redirectToHomePage()
-        }
-    }, 1000)
-
-    // 自动选择团队空间
-    dynamicQuery(
-        '[role="dialog"]',
-        e =>
-            e.lastElementChild.firstElementChild.firstElementChild.firstElementChild.click(),
-        { timeout: 2000 }
-    )
-} else if (location.href.includes('https://aizex.net')) {
-    // 共享池页面
-
-    setTimeout(() => {
-        // 如果未登录，首页会有一个登录/注册按钮
-        window.a = textQuery('登录/注册')
-        if (a.length > 0) {
+  setTimeout(() => {
+    // 如果未登录，首页会有一个登录/注册按钮
+    window.a = textQuery('登录/注册')
+    if (a.length > 0) {
+      a[0].click()
+    } else {
+      // 这是已登录的情况
+      onUrlChange(url => {
+        // console.log('URL changed to:', url)
+        if (url === gptPoolUrl) {
+          // console.log('Executing function...')
+          setTimeout(() => {
+            window.a = textQuery('Team')
             a[0].click()
-        } else {
-            // 这是已登录的情况
-            onUrlChange(url => {
-                // console.log('URL changed to:', url)
-                if (url === 'https://aizex.net/plusPool') {
-                    // console.log('Executing function...')
-                    setTimeout(() => {
-                        window.a = textQuery('Team')
-                        a[0].click()
-                        setTimeout(() => {
-                            location.href = 'https://panter.aizex.cn/'
-                        }, 2000)
-                    }, 1000)
-                }
-            })
+            setTimeout(() => {
+              location.href = gptChatUrl
+            }, 2000)
+          }, 1000)
         }
-    }, 1000)
+      })
+    }
+  }, 1000)
+} else if (location.pathname !== '/api/auth/session') {
+  // 对话页面
+
+  // 如果未登录，则会有一个"登录失效"的文本，这时直接重定向到首页
+  setTimeout(() => {
+    if (textQuery('登录失效').length > 0) {
+      location.href = gptPoolUrl
+    }
+  }, 2000)
+
+  // 自动选择团队空间
+  dynamicQuery(
+    '[role="dialog"]',
+    e =>
+      e.lastElementChild.firstElementChild.firstElementChild.firstElementChild.click(),
+    { timeout: 2000 }
+  )
+
+  // 检测鼠标在 2s 内是否有移动，如果没有则关闭页面
+  listenerWithTimeout(() => window.close(), 2000, 'mousemove')
 }
--- utils.ts
+++ utils.ts
@@ -13,146 +13,200 @@ import { build } from '../usbuild'
  * // falseArray = [1, 3, 5]
  */
 export function groupBy<T>(array: T[], f: (item: T) => boolean): [T[], T[]] {
-    const trueArray: T[] = []
-    const falseArray: T[] = []
-    array.forEach(item => {
-        ; (f(item) ? trueArray : falseArray).push(item)
-    })
-    return [trueArray, falseArray]
+  const trueArray: T[] = []
+  const falseArray: T[] = []
+  array.forEach(item => {
+    ;(f(item) ? trueArray : falseArray).push(item)
+  })
+  return [trueArray, falseArray]
 }
 
 export function sleep(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms))
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 /**
  * @description 仅隐藏元素, 它仍然存在于 DOM 树中
  */
 export function hideElements(element: ElementCSSInlineStyle) {
-    element.style.display = 'none'
+  element.style.display = 'none'
 }
 
 /**
  * @description 从 DOM 中完全移除元素
  */
 export function removeElement(element: ChildNode) {
-    element.remove()
+  element.remove()
 }
 
 export function CSSJustifyCenter(element: ElementCSSInlineStyle) {
-    element.style['justify-content'] = 'center'
+  element.style['justify-content'] = 'center'
 }
 
 // See https://bbs.tampermonkey.net.cn/thread-2895-1-1.html
-export function addMessageListener(listener, trustedOrigin?: string | string[]) {
-    const trustedOrigins = Array.isArray(trustedOrigin) ? trustedOrigin : [trustedOrigin]
-    window.addEventListener('message', e => {
-        if (
-            trustedOrigin === undefined ||
-            trustedOrigins.includes(e.origin)
-        ) {
-            listener(e.data, e)
-        }
-    })
+export function addMessageListener(
+  listener,
+  trustedOrigin?: string | string[]
+) {
+  const trustedOrigins = Array.isArray(trustedOrigin)
+    ? trustedOrigin
+    : [trustedOrigin]
+  window.addEventListener('message', e => {
+    if (trustedOrigin === undefined || trustedOrigins.includes(e.origin)) {
+      listener(e.data, e)
+    }
+  })
 }
 
 export function send(message: string, options: any = {}) {
-    const { targetwindow, windowProperty = 'parent', iframeSelector, targetOrigin = '*', transfer } = options
+  const {
+    targetwindow,
+    windowProperty = 'parent',
+    iframeSelector,
+    targetOrigin = '*',
+    transfer,
+  } = options
 
-    // 默认使用 window.parent 作为 targetWindow
-    const finalTargetWindow: Window | undefined = targetwindow ??
-        (iframeSelector
-            ? document.querySelector(iframeSelector)?.contentWindow
-            : window[windowProperty])
+  // 默认使用 window.parent 作为 targetWindow
+  const finalTargetWindow: Window | undefined =
+    targetwindow ??
+    (iframeSelector
+      ? document.querySelector(iframeSelector)?.contentWindow
+      : window[windowProperty])
 
-    if (finalTargetWindow?.postMessage) {
-        finalTargetWindow.postMessage(message, targetOrigin, transfer)
-    }
-    else {
-        console.error('postMessage Error!')
-    }
+  if (finalTargetWindow?.postMessage) {
+    finalTargetWindow.postMessage(message, targetOrigin, transfer)
+  } else {
+    console.error('postMessage Error!')
+  }
 }
 
 export function isNil(value) {
-    return value === undefined || value === null
+  return value === undefined || value === null
 }
 
 export function isEmptyString(str) {
-    return isNil(str) || str === ''
+  return isNil(str) || str === ''
 }
 
 /**
  *  @description 根据当前页面的路径，判断是否执行 f 函数
  */
 export function switchPath(path: string, f: () => void) {
-    if (path.startsWith('/')) {
-        console.error('path should not start with /')
-        return
-    }
-    if (window.location.pathname.split('/')[1].trim() === path) {
-        f()
-    }
+  if (path.startsWith('/')) {
+    console.error('path should not start with /')
+    return
+  }
+  if (window.location.pathname.split('/')[1].trim() === path) {
+    f()
+  }
 }
 
 export const globalConfig = {
-    namespace: 'L-UserScript',
-    version: '0.1.0',
-    author: 'Lin',
-    license: 'MIT License',
-    source: 'https://github.com/LinLin00000000/L-UserScript',
-    description: "Lin's userscript. 喵~",
+  namespace: 'L-UserScript',
+  version: '0.1.0',
+  author: 'Lin',
+  license: 'MIT License',
+  source: 'https://github.com/LinLin00000000/L-UserScript',
+  description: "Lin's userscript. 喵~",
 }
 
 export function mybuild(...args: Parameters<typeof build>) {
-    return build({
-        ...globalConfig,
-        ...args[0],
-    }, args[1])
+  return build(
+    {
+      ...globalConfig,
+      ...args[0],
+    },
+    args[1]
+  )
 }
 
 export function addClass(e: Element, cs: string) {
-    cs.split(' ').forEach(c => e.classList.add(c))
+  cs.split(' ').forEach(c => e.classList.add(c))
 }
 
 export function textQuery(text: string): HTMLElement[] {
-    // 创建一个 XPath 表达式来查找包含指定文本的元素
-    const xpath = `//*[contains(text(), '${text}')]`
+  // 创建一个 XPath 表达式来查找包含指定文本的元素
+  const xpath = `//*[contains(text(), '${text}')]`
 
-    // 使用 document.evaluate 执行 XPath 表达式
-    const result = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null)
+  // 使用 document.evaluate 执行 XPath 表达式
+  const result = document.evaluate(
+    xpath,
+    document,
+    null,
+    XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+    null
+  )
 
-    // 创建一个数组来存储找到的所有元素
-    const elements: HTMLElement[] = []
-    for (let i = 0; i < result.snapshotLength; i++) {
-        const elem = result.snapshotItem(i)
-        if (elem instanceof HTMLElement) {
-            elements.push(elem)
-        }
+  // 创建一个数组来存储找到的所有元素
+  const elements: HTMLElement[] = []
+  for (let i = 0; i < result.snapshotLength; i++) {
+    const elem = result.snapshotItem(i)
+    if (elem instanceof HTMLElement) {
+      elements.push(elem)
     }
+  }
 
-    return elements
+  return elements
 }
 
 export function onUrlChange(callback: (arg0: string) => void) {
-    // 初始调用，检查当前 URL 是否符合条件
+  // 初始调用，检查当前 URL 是否符合条件
+  callback(window.location.href)
+
+  // 监听 `popstate` 事件
+  window.addEventListener('popstate', () => {
     callback(window.location.href)
+  })
 
-    // 监听 `popstate` 事件
-    window.addEventListener('popstate', () => {
-        callback(window.location.href)
-    })
+  // 覆盖 `pushState` 和 `replaceState` 方法，以便监听 URL 变化
+  const originalPushState = history.pushState
+  const originalReplaceState = history.replaceState
 
-    // 覆盖 `pushState` 和 `replaceState` 方法，以便监听 URL 变化
-    const originalPushState = history.pushState
-    const originalReplaceState = history.replaceState
+  history.pushState = function () {
+    originalPushState.apply(history, arguments)
+    callback(window.location.href)
+  }
 
-    history.pushState = function () {
-        originalPushState.apply(history, arguments)
-        callback(window.location.href)
+  history.replaceState = function () {
+    originalReplaceState.apply(history, arguments)
+    callback(window.location.href)
+  }
+}
+
+export function listenerWithTimeout(
+  timeoutCallback: () => void,
+  timeoutDuration: number,
+  eventName: Parameters<typeof window.addEventListener>[0],
+  options: Parameters<typeof window.addEventListener>[2] = { once: true }
+): void {
+  let timer = setTimeout(timeoutCallback, timeoutDuration)
+
+  window.addEventListener(
+    eventName,
+    () => {
+      clearTimeout(timer)
+    },
+    options
+  )
+}
+
+/**
+ * 从给定的 URL 数组中提取唯一的主机名，并为每个主机名添加 "/*" 后缀
+ * @param urls - 要处理的 URL 数组
+ * @returns 唯一主机名数组，每个都以 "/*" 结尾
+ */
+export function extractUniqueHosts(urls: string[]): string[] {
+  const uniqueHosts = new Set<string>()
+
+  for (const url of urls) {
+    try {
+      const parsedUrl = new URL(url)
+      uniqueHosts.add(parsedUrl.host)
+    } catch (error) {
+      console.error(`Invalid URL: ${url}`)
     }
+  }
 
-    history.replaceState = function () {
-        originalReplaceState.apply(history, arguments)
-        callback(window.location.href)
-    }
+  return Array.from(uniqueHosts).map(host => `${host}/*`)
 }
