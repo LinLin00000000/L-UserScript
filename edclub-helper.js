28b8cae65a2059f0c223c7e5b260161f5af22ed8
Lin <i@linlin.me>
2024-10-31 10:22:31 +0800

    feat: Enhance EdClub helper script to handle Ctrl + L shortcut for refreshing

--- edclub-helper.js
+++ edclub-helper.js
@@ -1,9 +1,20 @@
 import { dynamicQuery, mybuild } from './utils'
-await mybuild({
-  match: ['https://www.edclub.com/sportal/*'],
-})
 
-// Example functionality: Log a message on page load
-dynamicQuery('body', () => {
-  console.log('EdClub Helper script loaded!')
+await mybuild(
+  {
+    match: ['https://www.edclub.com/sportal/*'],
+  },
+  {
+    // dev: true,
+  }
+)
+
+// Handle Ctrl + L shortcut
+document.addEventListener('keydown', event => {
+  if (event.ctrlKey && event.key === 'l') {
+    event.preventDefault()
+    dynamicQuery('.edicon-refresh, .navbar-goback', element => {
+      element.click()
+    })
+  }
 })
